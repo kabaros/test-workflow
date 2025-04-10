6f8075e441e871c28ee9049e75c3a675a1f56351
Mozafar Haider <mozafar@dhis2.org>
2025-04-10 07:57:10 +0100

    feat: setup ci

---
+++ d2.config.js
@@ -0,0 +1,8 @@
+const config = {
+    name: 'test-workflow',
+    type: 'app',
+    title: 'Test workflow',
+    minDHIS2Version: '2.40',
+}
+
+module.exports = config
---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+const App = () => {
+    return <div>Test workflow</div>
+}
+
+export default App
